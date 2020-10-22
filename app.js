d1dfff8b3f09b128e0adf8495bf1b697060ff89e
Esperanza Cardenas <60672335+Esperac25@users.noreply.github.com>
2020-10-22 07:01:52 -0700

    Update app.js
    fixed typo in startPageTemplate - h2, from "US" to "USA" to match the page title.

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ function startPageTemplate() {
   // define variable to hold wireframe start page html
   // return that variable
   let startPage = `  <div class ="container">
-    <h2 id="title">US Geography Quiz</h2>
+    <h2 id="title">USA Geography Quiz</h2>
     <button id="start">New Quiz</button>
   </div>`;
   return startPage;
